b640279b116a3e8135c9d87f9ee907b1ba51a83d
yuiuae <Serhii_Khrystenko@epam.com>
2023-05-22 15:26:40 +0300

    Trying to fix the heroku error 3

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	// "UserCreate" and "CheckUser" are handler that we will implement
 	http.HandleFunc("/user", handlers.UserCreate)
-	// http.HandleFunc("/user/login", handlers.UserLogin)
-	// http.HandleFunc("/admin", handlers.GetUserAll)
+	http.HandleFunc("/user/login", handlers.UserLogin)
+	http.HandleFunc("/admin", handlers.GetUserAll)
 
 	// start the server on port 8000
 	log.Fatal(http.ListenAndServe(":8000", nil))
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,7 +10,13 @@ documentation and does not have any functionality:)
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/yuiuae/HTTPServer/pkg/hasher"
 )
 
 // calls per hour allowed by the user
@@ -42,58 +48,54 @@ type CrResponse struct {
 
 // Create new use and add to user table
 func UserCreate(w http.ResponseWriter, r *http.Request) {
-	// if r.Method != "POST" {
-	// 	http.Error(w, "Only POST method allowed", http.StatusBadRequest)
-	// 	return
-	// }
-	// req := &CrRequest{}
-	// err := json.NewDecoder(r.Body).Decode(req)
-	// if err != nil {
-	// 	http.Error(w, "Bad request, empty username or password", http.StatusBadRequest)
-	// 	return
-	// }
-	// if len(req.UserName) <= 4 {
-	// 	http.Error(w, "Username should be at least 4 characters", http.StatusBadRequest)
-	// 	return
-	// }
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method allowed", http.StatusBadRequest)
+		return
+	}
+	req := &CrRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		http.Error(w, "Bad request, empty username or password", http.StatusBadRequest)
+		return
+	}
+	if len(req.UserName) <= 4 {
+		http.Error(w, "Username should be at least 4 characters", http.StatusBadRequest)
+		return
+	}
 
-	// if len(req.Password) <= 8 {
-	// 	http.Error(w, "Password should be at least 8 characters", http.StatusBadRequest)
-	// 	return
-	// }
-	// _, b := usersTable[req.UserName]
-	// if b {
-	// 	http.Error(w, "A user with this name already exists", http.StatusConflict)
-	// 	return
-	// }
+	if len(req.Password) <= 8 {
+		http.Error(w, "Password should be at least 8 characters", http.StatusBadRequest)
+		return
+	}
+	_, b := usersTable[req.UserName]
+	if b {
+		http.Error(w, "A user with this name already exists", http.StatusConflict)
+		return
+	}
 
-	// // Hash the password using the bcrypt algorithm
-	// hashedPassword, err := hasher.HashPassword(req.Password)
-	// if err != nil {
-	// 	http.Error(w, "Internal Server Error (hash error)", http.StatusInternalServerError)
-	// 	return
-	// }
+	// Hash the password using the bcrypt algorithm
+	hashedPassword, err := hasher.HashPassword(req.Password)
+	if err != nil {
+		http.Error(w, "Internal Server Error (hash error)", http.StatusInternalServerError)
+		return
+	}
 
-	// // Generate UUID
-	// uid, err := uuid.NewUUID()
-	// if err != nil {
-	// 	http.Error(w, "Internal Server Error (UUID error)", http.StatusInternalServerError)
-	// 	return
-	// }
+	// Generate UUID
+	uid, err := uuid.NewUUID()
+	if err != nil {
+		http.Error(w, "Internal Server Error (UUID error)", http.StatusInternalServerError)
+		return
+	}
 
-	// // Create response
-	// resp := &CrResponse{uid.String(), req.UserName}
-	// err = json.NewEncoder(w).Encode(&resp) //&resp
-	// if err != nil {
-	// 	http.Error(w, "Internal Server Error (json Encoder error)", http.StatusInternalServerError)
-	// 	return
-	// }
-	// // add new user to to user table
-	// usersTable[req.UserName] = UserInfo{hashedPassword, uid.String()}
-	/*
-		req := &CrRequest{}
-		json.NewDecoder(r.Body).Decode(req)
-	*/
+	// Create response
+	resp := &CrResponse{uid.String(), req.UserName}
+	err = json.NewEncoder(w).Encode(&resp) //&resp
+	if err != nil {
+		http.Error(w, "Internal Server Error (json Encoder error)", http.StatusInternalServerError)
+		return
+	}
+	// add new user to to user table
+	usersTable[req.UserName] = UserInfo{hashedPassword, uid.String()}
 }
 
 // Create a struct that models the structure for a user login
@@ -110,38 +112,30 @@ type LogResponse struct {
 
 // Check username and passoword in user table
 func UserLogin(w http.ResponseWriter, r *http.Request) {
-	// if r.Method != "POST" {
-	// 	http.Error(w, "Only POST method allowed", http.StatusBadRequest)
-	// 	return
-	// }
-	// req := &LogRequest{}
-	// err := json.NewDecoder(r.Body).Decode(req)
-	// if err != nil {
-	// 	http.Error(w, "Bad request, empty username or password", http.StatusBadRequest)
-	// 	return
-	// }
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method allowed", http.StatusBadRequest)
+		return
+	}
+	req := &LogRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		http.Error(w, "Bad request, empty username or password", http.StatusBadRequest)
+		return
+	}
 
-	// ok := hasher.CheckPasswordHash(usersTable[req.UserName].Passhash, req.Password)
-	// if !ok {
-	// 	http.Error(w, "Invalid username/password", http.StatusBadRequest)
-	// 	return
-	// }
+	ok := hasher.CheckPasswordHash(usersTable[req.UserName].Passhash, req.Password)
+	if !ok {
+		http.Error(w, "Invalid username/password", http.StatusBadRequest)
+		return
+	}
 
-	// url := "ws://fancy-chat.io/ws&token=one-time-token"
-	// resp := &LogResponse{url}
-	// w.Header().Add("X-Rate-Limit", strconv.Itoa(callperhour))
-	// w.Header().Add("X-Expires-After", time.Now().UTC().Add(time.Hour*time.Duration(tokentime)).String())
-	// err = json.NewEncoder(w).Encode(resp)
-	// if err != nil {
-	// 	http.Error(w, "Internal Server Error (json Encoder)", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	/*
-		req := &LogRequest{}
-		json.NewDecoder(r.Body).Decode(req)
-		url := "ws://fancy-chat.io/ws&token=one-time-token"
-		resp := &LogResponse{url}
-		json.NewEncoder(w).Encode(resp)
-	*/
+	url := "ws://fancy-chat.io/ws&token=one-time-token"
+	resp := &LogResponse{url}
+	w.Header().Add("X-Rate-Limit", strconv.Itoa(callperhour))
+	w.Header().Add("X-Expires-After", time.Now().UTC().Add(time.Hour*time.Duration(tokentime)).String())
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, "Internal Server Error (json Encoder)", http.StatusInternalServerError)
+		return
+	}
 }
